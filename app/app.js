dbacec2db85633a46013c537178bb43669adc230
Ivan <ivanhassay@gmail.com>
2018-04-19 00:10:23 +0300

    login ane authorization without session

--- app/app.js
+++ app/app.js
@@ -1,9 +1,48 @@
-const express = require('express');
-const app = express();
-const db = require('./config/dbConnect');
+import express from 'express';
+import session from 'express-session'; 
+import bodyParser from 'body-parser';
+import routes from './routes/routes';
+import db from './config/dbConnect';
 
-app.get('/', (req, res) => {
-   res.send('Hello World!');
+const app = express();
+// const db = require('./config/dbConnect');
+
+
+// View engine setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+app.disable('x-powered-by');
+app.set('trust proxy', 1);
+app.set('view engine', 'pug');
+
+// Routes
+app.use('/', routes);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// Error handler
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  if (err.status === 400) {
+    res.json({
+      error: err.message,
+    });
+  } else {
+    res.json({ error: { message: err.message } });
+  }
 });
 
 app.listen(3000, () => {
---
+++ app/routes/routes.js
@@ -0,0 +1,53 @@
+import { Router } from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../schems/UserSchems';
+
+const routes = Router();
+routes.get('/', (req, res) => {
+  res.send({ index: 2 });
+});
+
+
+// USER ENDPOINTS
+routes.get('/users', (req, res) => {
+  User.find({}, (err, users) => {
+    res.json(users);
+  });
+}); 
+
+
+routes.post('/setup', (req, res) => {
+  // create a sample user
+  console.warn('req', req.body);
+  const nick = new User({ 
+    username: req.body.username, 
+    password: req.body.password,
+  });
+  // save the sample user
+  nick.save((err) => {
+    if (err) throw err;
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+
+routes.post('/authenticate', (req, res) => {
+  User.findOne({
+    username: req.body.username
+  }, (err, user) => {
+    if (err) throw err;
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+      if (user.password !== req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+        res.json({
+          token: jwt.sign({ username: user.username, password: user.password }, 'RESTFULAPIs') });
+      }
+    }
+  });
+});
+
+export default routes;
